188bf9f5d351489fd09388074e5bb3066d7efe55
Davi <davifssoares2002@gmail.com>
2024-07-18 21:44:31 -0300

    feat: finalizing register user integration

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -2,7 +2,6 @@ import styles from "./style.module.scss";
 
 const Input = ({labelName, placeholder, value, handleOnChange}) => {
     return <div className={styles.inputContainer}>
-        <label>{labelName}</label>
         <input placeholder={placeholder} onChange={handleOnChange} value={value}/>
     </div>
 };
--- src/views/registerDriver/RegisterDriver.jsx
+++ src/views/registerDriver/RegisterDriver.jsx
@@ -6,9 +6,12 @@ import Input from "../../components/input/Input";
 import { getAddressInfosByCEP } from "../../services/cepService";
 import { toast } from "react-toastify";
 import toastConfigs from "../../utils/toastConfigs";
+import { createUser } from "../../services/userService";
+import { userTypeEnum } from "../../utils/userTypeEnum";
 
 const RegisterDriver = () => {
     const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
     const [cnh, setCNH] = useState("");
     const [cpf, setCPF] = useState("");
     const [rg, setRG] = useState("");
@@ -23,6 +26,10 @@ const RegisterDriver = () => {
 
     const handleName = (e) => {
         setName(e.target.value);
+    };
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
     };
 
     const handleCNH = (e) => {
@@ -50,7 +57,7 @@ const RegisterDriver = () => {
             toast.error("Digite um CEP válido", toastConfigs);
             return;
         }
-        
+
         const request = await getAddressInfosByCEP(cep);
 
         setStreet(request.logradouro);
@@ -79,6 +86,82 @@ const RegisterDriver = () => {
         setState(e.target.value);
     };
 
+    const verifyFieldsFilled = () => {
+        if(name.length === 0) return;
+
+        if(email.length === 0) return;
+
+        if(cnh.length === 0) return;
+
+        if(cpf.length === 0) return;
+
+        if(rg.length === 0) return;
+
+        if(phone.length === 0) return;
+
+        if(cep.length === 0) return;
+
+        if(street.length === 0) return;
+
+        if(number.length === 0) return;
+
+        if(neighborhood.length === 0) return;
+
+        if(city.length === 0) return;
+        
+        if(state.length === 0) return;
+    };
+
+    const cleanAllFields = () => {
+        setName("");
+        setEmail("");
+        setCNH("");
+        setCPF("");
+        setRG("");
+        setPhone("");
+        setCep("");
+        setStreet("");
+        setNumber("");
+        setNeighborhood("");
+        setCity("");
+        setState("");
+    };
+
+    const handleRegister = async() => {
+        const isAllFilled = verifyFieldsFilled();
+
+        if(!isAllFilled){
+            toast.error("Preencha todos os dados corretamente", toastConfigs);
+            return;
+        }
+
+        const body = {
+            name: name,
+            email: email,
+            cpf: cpf.replace(/\D/g, ''),
+            cnh: cnh,
+            rg: rg,
+            phone: phone.replace(/\D/g, ''),
+            user_type_id: userTypeEnum.MOTORISTA,
+            address: {
+                address: `${street}, ${number}`, 
+                city: city, 
+                neighborhood: neighborhood, 
+                state: state
+            }
+        };
+        
+        const response = await createUser(body);
+
+        if(response.status === 201){
+            toast.success(response.data, toastConfigs);
+            cleanAllFields();
+        }
+        else{
+            toast.error(response.data, toastConfigs);
+        }
+    };
+
     return <>
         <NavBar optionSelected={2}/>
         <div className={styles.container}>
@@ -87,34 +170,37 @@ const RegisterDriver = () => {
             <div className={styles.userFieldsContainer}>
                 <h4>Dados do Usuário</h4>
                 <Row>
-                    <Input labelName="Nome" placeholder="Digite o nome" handleOnChange={handleName} value={name}/>
-                    <Input labelName="CNH" placeholder="Digite a CNH" handleOnChange={handleCNH} value={cnh}/>
+                    <Input placeholder="Nome" handleOnChange={handleName} value={name}/>
+                    <Input placeholder="Email" handleOnChange={handleEmail} value={email}/>
                 </Row>      
                 <Row>
-                    <Input labelName="CPF" placeholder="Digite o CPF" handleOnChange={handleCPF} value={cpf}/>
-                    <Input labelName="RG" placeholder="Digite o RG" handleOnChange={handleRG} value={rg}/>
+                    <Input placeholder="CNH" handleOnChange={handleCNH} value={cnh}/>
+                    <Input placeholder="CPF" handleOnChange={handleCPF} value={cpf}/>
                 </Row>    
                 <Row>
-                    <Input labelName="Telefone" placeholder="Digite o telefone" handleOnChange={handlePhone} value={phone}/>
+                    <Input placeholder="RG" handleOnChange={handleRG} value={rg}/>
+                    <Input placeholder="Telefone" handleOnChange={handlePhone} value={phone}/>
                 </Row>              
             </div>
 
             <div className={styles.userFieldsContainer}>
                 <h4>Dados do Endereço</h4>
                 <Row>
-                    <Input labelName="CEP" placeholder="Digite o CEP" handleOnChange={handleCEP} value={cep}/>
+                    <Input placeholder="CEP" handleOnChange={handleCEP} value={cep}/>
                     <button onClick={handleSearchCEP}>buscar</button>
-                    <Input labelName="Rua" placeholder="Digite a rua" handleOnChange={handleStreet} value={street}/>
+                    <Input placeholder="Rua" handleOnChange={handleStreet} value={street}/>
                 </Row>      
                 <Row>
-                    <Input labelName="Número" placeholder="Digite o número" handleOnChange={handleNumber} value={number}/>
-                    <Input labelName="Bairro" placeholder="Digite o bairro" handleOnChange={handleNeighborhood} value={neighborhood}/>
+                    <Input placeholder="Número" handleOnChange={handleNumber} value={number}/>
+                    <Input placeholder="Bairro" handleOnChange={handleNeighborhood} value={neighborhood}/>
                 </Row>    
                 <Row>
-                    <Input labelName="Cidade" placeholder="Digite a cidade" handleOnChange={handleCity} value={city}/>
-                    <Input labelName="Estado" placeholder="Digite o estado" handleOnChange={handleState} value={state}/>
+                    <Input placeholder="Cidade" handleOnChange={handleCity} value={city}/>
+                    <Input placeholder="Estado" handleOnChange={handleState} value={state}/>
                 </Row>     
             </div>
+
+            <button onClick={handleRegister} className={styles.buttonRegister}>Enviar</button>
         </div>
     </>
 };
